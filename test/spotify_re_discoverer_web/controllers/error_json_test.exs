9dc4c154736ba5616ffc00b8a57ef0e548397c71
Mike Bowman <mike@mikebowman.dev>
2023-06-20 16:58:59 -0400

    Format

--- test/spotify_re_discoverer_web/controllers/error_json_test.exs
+++ test/spotify_re_discoverer_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule SpotifyReDiscovererWeb.ErrorJSONTest do
   use SpotifyReDiscovererWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert SpotifyReDiscovererWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert SpotifyReDiscovererWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do

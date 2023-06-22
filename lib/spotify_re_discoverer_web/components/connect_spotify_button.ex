1cf8d15dc9ee7db78c794893bbcf91fb88e66175
Mike Bowman <mike@mikebowman.dev>
2023-06-21 20:55:55 -0400

    Trivial change

--- lib/spotify_re_discoverer_web/components/connect_spotify_button.ex
+++ lib/spotify_re_discoverer_web/components/connect_spotify_button.ex
@@ -6,12 +6,21 @@ defmodule SpotifyReDiscovererWeb.Components.ConnectSpotifyButton do
   def render(assigns) do
     ~H"""
     <div>
+      <p>Count: <%= @count %></p>
       <.button type="button" phx-click="click" phx-target={@myself}>Connect To Spotify</.button>
     </div>
     """
   end
 
+  def mount(socket) do
+    socket
+    |> assign(:count, 0)
+    |> reply_ok()
+  end
+
   def handle_event("click", _params, socket) do
-    noreply(socket)
+    socket
+    |> update(:count, &(&1 + 1))
+    |> noreply()
   end
 end

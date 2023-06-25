ea074a6d0a3b591ad1b80dafec8ce809cd8615a5
Mike Bowman <mike@mikebowman.dev>
2023-06-25 19:28:23 -0400

    Fetch playlists in liveview

--- lib/spotify_re_discoverer_web/live/dashboard_live.ex
+++ lib/spotify_re_discoverer_web/live/dashboard_live.ex
@@ -3,6 +3,7 @@ defmodule SpotifyReDiscovererWeb.DashboardLive do
 
   alias SpotifyReDiscovererWeb.Components.ConnectSpotifyButton
   alias SpotifyReDiscoverer.Accounts
+  alias SpotifyReDiscoverer.Spotify.Client
 
   def render(assigns) do
     ~H"""
@@ -10,6 +11,13 @@ defmodule SpotifyReDiscovererWeb.DashboardLive do
     <%= if @current_user do %>
       <p><%= @current_user.email %></p>
       <.live_component module={ConnectSpotifyButton} id="spotify-button-dashboard" />
+
+      <.button phx-click="get_playlists">Fetch Playlists</.button>
+      <ul>
+        <%= for playlist <- @playlists do %>
+          <li><%= playlist["name"] %></li>
+        <% end %>
+      </ul>
     <% end %>
     """
   end
@@ -19,6 +27,15 @@ defmodule SpotifyReDiscovererWeb.DashboardLive do
 
     socket
     |> assign(:current_user, current_user)
+    |> assign(:playlists, [])
     |> reply_ok()
+  end
+
+  def handle_event("get_playlists", _params, socket) do
+    playlists = Client.get_all_playlists(socket.assigns.current_user)
+
+    socket
+    |> assign(:playlists, Map.get(playlists, "items"))
+    |> noreply()
   end
 end
